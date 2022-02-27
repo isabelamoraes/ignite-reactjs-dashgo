bd2478fe886c696d43829e378b03b3670c2f7a08
Isabela Moraes <isabelagmoraes@hotmail.com>
2022-02-27 17:12:05 -0300

    Upload to git

---
+++ src/components/Sidebar/NavSection.tsx
@@ -0,0 +1,20 @@
+import { Box, Icon, Stack, Text } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface NvaSectionProps {
+  title: string;
+  children: ReactNode;
+}
+
+export function NavSection({ title, children }: NvaSectionProps) {
+  return (
+    <Box>
+      <Text fontWeight="bold" color="gray.400" fontSize="small">
+        {title}
+      </Text>
+      <Stack spacing="4" mt="8" align="stretch">
+        {children}
+      </Stack>
+    </Box>
+  );
+}
